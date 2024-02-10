b82a046576d06fefa0c47dd4a01e680fe84f3ae3
UmeshGayashan <umeshgayashan099@gmail.com>
2024-02-10 21:43:10 +0530

    Junit initialization

---
+++ src/JunitClass.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+public class JunitClass {
+	
+	@Test
+	public void JunitMethod() {
+		System.out.print("Executing Junit Testing");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+}
